099417bd9abebd98ed19dd3eb6f428162626a4c1
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-17 09:30:46 -0600

    Fetch all reservations

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    };
+  }
+
+  componentDidMount() {
+    this.getAllReservations();
+  }
+
+  getAllReservations() {
+    return fetch("http://localhost:3001/api/v1/reservations").then(response => {
+      if (!response.ok) {
+        throw Error("Error fetching reservations, please try again later.");
+      }
+      return response
+        .json()
+        .then(reservations => this.setState({ reservations: reservations }));
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <h1 className="app-title">Turing Cafe Reservations</h1>
+        <div className="resy-form"></div>
+        <div className="resy-container"></div>
       </div>
-    )
+    );
   }
 }
 
